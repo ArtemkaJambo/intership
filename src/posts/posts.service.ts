99a0fd5eeef0ab8cd705d0d470bcca35e9c0934b
Artem Dmysh <admysh2023@itcollege.lviv.ua>
2025-04-30 10:45:32 +0300

    fix(posts) add transaction

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -126,10 +126,10 @@ export class PostsService {
             throw new BadRequestException('only admin and owner post is able to delete post')
           }
      
-          await this.prisma.comment.deleteMany({ where: { postId: id } });
-          
-
-          await this.prisma.post.delete({ where: { id } });
+          await this.prisma.$transaction([
+            this.prisma.comment.deleteMany({ where: { postId: id } }),
+            this.prisma.post.delete({ where: { id } })
+        ])
 
 
           return {message: 'Post successfully deleted'}
