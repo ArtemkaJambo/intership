d10db7d1ce0b5270f5ba6cc5832cd7735af4466b
Artem Dmysh <admysh2023@itcollege.lviv.ua>
2025-06-03 15:46:14 +0300

    feat: add unfollow func

--- src/follower/follower.controller.ts
+++ src/follower/follower.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Param, ParseIntPipe, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Get, Param, ParseIntPipe, Post, Req, UseGuards } from '@nestjs/common';
 import { FollowerService } from './follower.service';
 import { JwtGuard } from 'src/auth/guard';
 import { RolesGuard } from 'src/auth/rolesDecorator/roles.guard';
@@ -16,7 +16,11 @@ export class FollowerController {
         return this.followerService.followed(id, userId)
     }
 
-    
+    @Delete(':id')  
+    unfollow(@Param('id', ParseIntPipe) id: number, @Req() req) {
+        const userId = req.user.id
+        return this.followerService.unfollow(id, userId)
+    }
 
 
 }
