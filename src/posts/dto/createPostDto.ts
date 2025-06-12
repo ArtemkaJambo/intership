a91f345b89c94aae0384cac67e840cd160e502ef
Artem Dmysh <admysh2023@itcollege.lviv.ua>
2025-06-12 21:45:23 +0300

    third-commit

--- src/posts/dto/createPostDto.ts
+++ src/posts/dto/createPostDto.ts
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import { IsBoolean, IsNotEmpty, IsOptional, IsString } from "class-validator";
-=======
->>>>>>> 459c1c717a91fa9ed6921d0c1f6409fdb3396413
 
 export class createPostDto {
     
@@ -17,10 +14,7 @@ export class createPostDto {
     @IsNotEmpty()
     archived: boolean
 
-<<<<<<< HEAD
     @IsOptional()
     @IsString()
-    postPhoto?: string | null  
+    postPhoto?: string | null
 }
-=======
->>>>>>> 459c1c717a91fa9ed6921d0c1f6409fdb3396413
